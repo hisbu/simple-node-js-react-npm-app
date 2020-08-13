2a3c317915edeb7c982c341e8905e1614b0aeaf9
hisbu <43975159+hisbu@users.noreply.github.com>
2020-08-14 06:53:14 +0700

    add clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Clock from 'react-live-clock';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header" style={{padding:"20px"}}>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Selamat datang di Website saya</h1>
           <h1 className="App-title">Alhamdulillah</h1>
@@ -14,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit dsgit<code>src/App.js</code> and save to reload.
         </p>
+          <Clock format={'HH:mm:ss'} ticking={true} timezone={'Asia/Jakarta'} />
+          <p>hallo</p>
       </div>
     );
   }
